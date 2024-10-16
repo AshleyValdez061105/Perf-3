c5840c78fc57be4aac49e34708030d9dd5bad706
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-10-16 19:34:57 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,16 @@
+// Create an array of favorite singers
+const favSingers = ["Adele", "Dua Lipa", "sarah"];
+
+// Log the first singer in the array
+console.log(favSingers[0]);
+
+// Create an array of favorite numbers
+const favNumbers = [11, 5, 16, 19 ];
+
+// Create an array with mixed data types
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); // string
+console.log(mixedArr[1]); // otherarray
+console.log(mixedArr[2]); // 123
+console.log(mixedArr[3]); // true
